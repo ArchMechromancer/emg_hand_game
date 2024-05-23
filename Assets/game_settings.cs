6d097421070b075583d0704c791f937272debbfc
Gabriel Rael <g.rael@hotmail.com>
2024-05-23 19:08:35 -0300

    menu_update
    Menus updated according to reviwer's sugestions. Bugfixes implemented (I hope)

--- Assets/game_settings.cs
+++ Assets/game_settings.cs
@@ -6,7 +6,14 @@ using TMPro;
 public class game_settings : MonoBehaviour
 {
     public float time_set = 300;
+    public float speed_set = 2;
+    public float interval_set = 2;
+    public float threshold_set = 3;
+
     public TextMeshProUGUI timeText;
+    public TextMeshProUGUI speedText;
+    public TextMeshProUGUI intervalText;
+    public TextMeshProUGUI thresholdText;
 
     public void SetThreshold (float threshold)
     {
@@ -31,5 +38,14 @@ public class game_settings : MonoBehaviour
         float minutes = Mathf.FloorToInt(time_set / 60);
         float seconds = Mathf.FloorToInt(time_set % 60);
         timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        speed_set = PlayerPrefs.GetFloat("CoinSpeed", 2);
+        speedText.text = speed_set.ToString();
+
+        interval_set = PlayerPrefs.GetFloat("CoinSpawn", 2);
+        intervalText.text = interval_set.ToString();
+
+        threshold_set = PlayerPrefs.GetFloat("threshhold", 3);
+        thresholdText.text = threshold_set.ToString();  
     }
 }
